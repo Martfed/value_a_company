54c35bad2ba19e2b656b7b5ed4b36736fd8a5463
Martfed <federicodm90@gmail.com>
2020-12-07 14:50:44 +0100

    extract invocation logic

--- Controller/index.js
+++ Controller/index.js
@@ -1,36 +1,23 @@
-const AWS = require('aws-sdk')
-const lambda = new AWS.Lambda({ region: 'eu-west-3' })
-
 const { returnOnEquity } = require('./src/return_on_equity')
+const { invokeBalanceSheet, invokeCashFlow } = require('./src/lambda_invocations')
 const headers = {
   'Content-Type': 'application/json',
   'Access-Control-Allow-Origin': '*'
 };
 
 exports.handler = async (event, context) => {
-  const balanceSheetParams = { 
-    FunctionName: 'balance-sheet',
-    Payload: JSON.stringify(event.queryStringParameters)
-  }
-  const cashFlowParams = {
-    FunctionName: 'cash-flow',
-    Payload: JSON.stringify(event.queryStringParameters)
-  }
-  const cashFlow = await new Promise((resolve, reject) => {
-    lambda.invoke(cashFlowParams, (error, data) => { resolve(data) })
-  })
-  const balanceSheet = await new Promise((resolve, reject) => {
-    lambda.invoke(balanceSheetParams, (error, data) => { resolve(data) })
-  })
+  const balanceSheet = await invokeBalanceSheet(event)
+  const cashFlow = await invokeCashFlow(event)
 
   const parsedBalanceSheet = JSON.parse(balanceSheet.Payload).body
+  const parsedCashFlow = JSON.parse(cashFlow.Payload).body
   return {
     statusCode: 200,
     headers,
     body: JSON.stringify({
       debtToEquity: parsedBalanceSheet.debtToEquity,
       currentRatio: parsedBalanceSheet.currentRatio,
-      returnOnEquity: returnOnEquity(cashFlow, parsedBalanceSheet.shareHoldersEquity)
+      returnOnEquity: returnOnEquity(parsedCashFlow, parsedBalanceSheet.shareHoldersEquity)
     })
   }
 }
--- Controller/src/return_on_equity.js
+++ Controller/src/return_on_equity.js
@@ -1,6 +1,5 @@
 const returnOnEquity = (cashflow, shareHoldersEquity) => {
-  const parsedPayload = JSON.parse(cashflow.Payload)
-  const roe = parsedPayload.body.netIncome.map((report, index) => {
+  const roe = cashflow.body.netIncome.map((report, index) => {
     report.totalShareholderEquity = shareHoldersEquity[index].shareHoldersEquity
     return report
   })
